423dec1b452a052de8ad51af1045f6dbb112b4b7
Tom <tomschmanke@gmx.de>
2022-05-16 22:17:17 +0200

    Refactor drink list into service

--- drinks-frontend/src/app/app.component.ts
+++ drinks-frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Categories } from './models/categories';
 import { DrinkModel } from './models/drink-model';
+import { DrinksService } from './shared/drinks.service';
 
 @Component({
   selector: 'app-root',
@@ -8,36 +9,15 @@ import { DrinkModel } from './models/drink-model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'drinks-frontend';
+  
+  drinks : DrinkModel[];
+  favourites: DrinkModel[];
+
   allCategories = Categories;
 
-  drinks : DrinkModel[] = [
-    {
-      category : Categories.UNFORGETTABLE,
-      name : "Mojito",
-      ingredients : ["4,5cl Weißer Kubanischer Rum", "2cl Limettensaft", "6 Minzblätter", "2 Barlöffel Rohrzucker", "4cl Sprudelwasser"],
-      recipe : ["Minze, Zucker und Limettensaft vermischen", "Etwas Sodawasser hinzufügen und Glaß mit Eis auffüllen", "Rum hingeben und mit Wasser auffüllen", "Leicht umrühren"],
-      toppings : ["Minzstängel", "Limttenscheiben"],
-      favourite : true
-    },
-    {
-      category : Categories.CLASSIC,
-      name : "Cosmopolitan",
-      ingredients : ["4cl Zitronenwodka", "1,5ml Cointreau", "1,5cl Limettensaft", "3cl Cranberrysaft"],
-      recipe : ["Alle Zutaten auf Eis shaken"],
-      toppings : ["Zitronenzeste"],
-      favourite : true
-    },
-    {
-      category : Categories.CLASSIC,
-      name : "Long Island Ice Tea",
-      ingredients : ["1,5cl Vodka", "1,5cl Tequila", "1,5cl Weißer Rum", "1,5ml Cointreau", "3cl Zitronensaft", "2cl Sirup", "Cola"],
-      recipe : ["Alle Zutaten mischen", "Auf Eis servieren"],
-      toppings : ["Zitronenscheibe"],
-      favourite : false
-    }
-  ].sort((a,b) => a.name.localeCompare(b.name));
-
-  favourites = this.drinks.filter((a) => a.favourite).slice(0, 4);
+  constructor(drinkService: DrinksService) {
+    this.drinks = drinkService.getAllDrinks();
+    this.favourites = drinkService.getFavoriteDrinks();
+  }
 
 }
---
+++ drinks-frontend/src/app/shared/drinks.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { Categories } from '../models/categories';
+import { DrinkModel } from '../models/drink-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DrinksService {
+
+  drinks: DrinkModel[];
+  
+  constructor() {
+    this.drinks = [
+      {
+        category : Categories.UNFORGETTABLE,
+        name : "Mojito",
+        ingredients : ["4,5cl Weißer Kubanischer Rum", "2cl Limettensaft", "6 Minzblätter", "2 Barlöffel Rohrzucker", "4cl Sprudelwasser"],
+        recipe : ["Minze, Zucker und Limettensaft vermischen", "Etwas Sodawasser hinzufügen und Glaß mit Eis auffüllen", "Rum hingeben und mit Wasser auffüllen", "Leicht umrühren"],
+        toppings : ["Minzstängel", "Limttenscheiben"],
+        favourite : true
+      },
+      {
+        category : Categories.CLASSIC,
+        name : "Cosmopolitan",
+        ingredients : ["4cl Zitronenwodka", "1,5ml Cointreau", "1,5cl Limettensaft", "3cl Cranberrysaft"],
+        recipe : ["Alle Zutaten auf Eis shaken", "In einem vorgekühlen Glaß servieren"],
+        toppings : ["Zitronenzeste"],
+        favourite : true
+      },
+      {
+        category : Categories.CLASSIC,
+        name : "Long Island Ice Tea",
+        ingredients : ["1,5cl Vodka", "1,5cl Tequila", "1,5cl Weißer Rum", "1,5cl Cointreau", "3cl Zitronensaft", "2cl Sirup", "Cola"],
+        recipe : ["Alle Zutaten mischen", "Auf Eis servieren"],
+        toppings : ["Zitronenscheibe"],
+        favourite : false
+      },
+      {
+        category : Categories.UNFORGETTABLE,
+        name : "Between the Sheets",
+        ingredients : ["3cl Weißer Rum", "3cl Cognac", "3cl Tripel Sec", "2cl Zitronensaft"],
+        recipe : ["Alle Zutaten auf Eis shaken", "In einem vorgekühlen Glaß servieren"],
+        toppings : ["N/A"],
+        favourite : false
+      }
+    ].sort((a,b) => a.name.localeCompare(b.name));
+   }
+
+   getAllDrinks(): DrinkModel[] {
+     return this.drinks;
+   }
+
+   getFavoriteDrinks(): DrinkModel[] {
+     return this.drinks.filter((a) => a.favourite).slice(0, 4);
+
+   }
+}
